d3f05dda94203a1c608d5ad72a79c99a25f172af
Marat <pmaratd@gmail.com>
2024-12-18 19:37:32 +0300

    Task 4 complete

--- collision/rectangles.py
+++ collision/rectangles.py
@@ -15,3 +15,17 @@ def isCollisionRect(rect1, rect2):
     (x2_min, y2_min), (x2_max, y2_max) = rect2
 
     return not (x1_max < x2_min or x1_min > x2_max or y1_max < y2_min or y1_min > y2_max)
+
+def intersectionAreaRect(rect1, rect2):
+    if not isCorrectRect(rect1):
+        raise ValueError("1й прямоугольник некоректный")
+    if not isCorrectRect(rect2):
+        raise ValueError("2й прямоугольник некоректный")
+
+    (x1_min, y1_min), (x1_max, y1_max) = rect1
+    (x2_min, y2_min), (x2_max, y2_max) = rect2
+
+    x_overlap = max(0, min(x1_max, x2_max) - max(x1_min, x2_min))
+    y_overlap = max(0, min(y1_max, y2_max) - max(y1_min, y2_min))
+
+    return x_overlap * y_overlap
